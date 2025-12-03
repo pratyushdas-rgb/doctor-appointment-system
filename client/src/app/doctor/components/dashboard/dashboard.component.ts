dcba115a7de835e9316aece812397717438fb27a
Pratyush Sundar Das <pratyush.das@argusoft.com>
2025-12-03 14:47:16 +0530

    code cleaning

--- client/src/app/doctor/components/dashboard/dashboard.component.ts
+++ client/src/app/doctor/components/dashboard/dashboard.component.ts
@@ -1,4 +1,3 @@
-// src/app/doctor/components/dashboard/dashboard.component.ts
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/auth/auth.service';
@@ -10,18 +9,8 @@ import { AuthService } from 'src/app/auth/auth.service';
 })
 export class DashboardComponent {
   user: any = null;
-
-  links = [
-    { label: 'Home', path: '/doctor' },
-    { label: 'Schedule Availability', path: '/doctor/schedule' }
-  ];
-
   constructor(private auth: AuthService, private router: Router) {
     this.user = this.auth.getUser();
   }
 
-  logout() {
-    this.auth.logout();
-    this.router.navigate(['/auth/login']);
-  }
 }
